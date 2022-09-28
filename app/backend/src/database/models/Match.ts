c58aa5e8a048ff8cb3b748bb099f31c2d6f6565a
fkrein1 <felipekrein@gmail.com>
2022-09-28 12:09:22 -0300

    feat: database models for migrations

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,50 @@
+import { BOOLEAN, INTEGER, Model } from 'sequelize';
+import Team from './Team';
+import db from '.';
+
+class Match extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teams' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teams' });
+
+export default Match;
